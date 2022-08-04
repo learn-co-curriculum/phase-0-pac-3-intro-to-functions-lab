15a01beef41b8c6084153d6550f147b29e3a5d85
peter <+>
2022-08-03 21:08:32 -0400

    commit tree 720dd53adc0cd96e981de963d8e55768c2049fb2

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout (string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    
+    console.log (string.toUpperCase("hello"));
+    
+}
+
+function logWhisper (string){
+    console.log(string.toLowerCase("HELLO"));
+}
+
+
+
+function sayHiToHeadphonedRoommate(string){
+    
+    if (string == string.toLowerCase()){
+
+        return 'I can\'t hear you!';
+    }
+
+
+    
+    if (string == string.toUpperCase()){
+        return "YES INDEED!";
+    }
+
+    if (string == "Let's have dinner together!") {
+         return "I would love to!";
+    }
+}
+
+
